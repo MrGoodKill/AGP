9b45fc6277bcab02e5b35cdef0cca30f2d0c5f1d
François Guthmann <francois.guthmann@telecom-paristech.fr>
2016-06-27 10:18:45 +0200

    Git messed up again

---
+++ src/Return2.java
@@ -0,0 +1,20 @@
+public class Return2 extends Node{
+	
+	private Var var;
+	
+	public Return2(Var var){
+		this.var = var;
+	}
+	
+	public String toASM(){
+		String output = ""
+		+ newLine("mov eax,[" + var.getName() + "]")
+		+ newLine("push eax")
+		+ newLine("mov eax,[funcReturn]")
+		+ newLine("push eax")
+		+ newLine("ret");
+		return output;
+	}
+	
+
+}
